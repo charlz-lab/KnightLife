47ff67a468e4b347831c3cddf4da19db06a2de2d
Jorge E Osorio G <jeosorio2012@gmail.com>
2024-01-24 12:46:48 -0500

    styles.js created and modified Navbar

--- styles.js
+++ styles.js
@@ -1,29 +1,32 @@
 const appStyles = {
-    colors: {
-        mainBackground:"#232323",
-        foreground:"#222222 ",
-        background:"#FFFFFF",
-        accent1:"#080808",
-        accent2:"#FFC60A"
+  colors: {
+    mainBackground: "#232323",
+    foreground: "#222222 ",
+    background: "#FFFFFF",
+    accent1: "#080808",
+    accent2: "#FFC60A",
+  },
+  buttons: {
+    borders: {
+      borderRadius: 20,
+      borderWidth: 1,
+      padding: 10,
+      backgroundColor: "#FFC60A",
     },
-    buttons:{
-        borders:{
-            borderRadius:20,
-            borderWidth:1,
-            padding:10,
-            backgroundColor:"#FFC60A"
-        }
+  },
+  fonts: {
+    paragraph: {
+      fontFamily: "IBMPlexSans-Medium",
+      fontSize: 15,
     },
-    fonts:{
-        ibmMedium:{
-            fontFamily:"IBMPlexSans-Medium",fontSize:15
-        },
-        ibmRegular:{
-            fontFamily:"IBMPlexSans-Regular",fontSize:15
-        },
-        prompt:{
-            fontFamily:"Prompt-Bold", fontSize:20
-        }
-    }
-}
-export default appStyles 
+    subHeading: {
+      fontFamily: "IBMPlexSans-Regular",
+      fontSize: 15,
+    },
+    heading: {
+      fontFamily: "Prompt-Bold",
+      fontSize: 20,
+    },
+  },
+};
+export default appStyles;
