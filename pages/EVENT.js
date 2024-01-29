ebfbc28ae96cb21754f82bda8c9a8e24b0b0b511
Cali Carpenter <calicarpenter07@gmail.com>
2024-01-29 18:12:46 -0500

    package

--- pages/EVENT.js
+++ pages/EVENT.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, ImageBackground, Image } from 'react-native';
 import events from '../components/EventList';
 import { Card, Icon } from 'react-native-elements';
 import { useRoute } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import appStyles from '../styles';
+import Modal from 'react-native-modal';
 
 const EventPage = ({ route, navigation }) => {
   const { event } = route.params;
-  //function to navigate to the previous page
   const handleBack = () => {
     navigation.goBack();
   };
+  //report modal usestate
+  const [isModalVisible, setModalVisible] = useState(false);
+  //toggle showing modal
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+  //function to navigate to the previous page
+
   return (
     <View style={styles.container}>
       <Image source={event.image} style={styles.image} />
@@ -21,7 +29,16 @@ const EventPage = ({ route, navigation }) => {
           type='ionicon'
           size={25}
           color="#676464"
-          style={styles.locationIcon}
+          style={styles.close}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={toggleModal} style={styles.reportButton}>
+        <Icon
+          name="alert-circle-outline"
+          type='ionicon'
+          size={25}
+          color="#FFC60A"
+          style={styles.report}
         />
       </TouchableOpacity>
       <Text style={styles.dateTime}>{event.dateTime}</Text>
@@ -45,7 +62,19 @@ const EventPage = ({ route, navigation }) => {
           <Card width="225" borderRadius={12} style={[styles.shadow, styles.card]}><Text>{event.description}</Text></Card>
         </View>
       </View>
-
+      <Modal isVisible={isModalVisible} onBackdropPress={toggleModal} style={styles.modal}>
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>Report Event</Text>
+          <View style={styles.modalOptionsContainer}>
+            <TouchableOpacity onPress={toggleModal} style={styles.modalOption}>
+              <Text>Report</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={toggleModal} style={styles.modalOption}>
+              <Text>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -63,6 +92,14 @@ const styles = StyleSheet.create({
     padding: 16,
     backgroundColor: '#fff',
     flex: 1,
+  },
+  reportButton: {
+    position: 'absolute',
+    top: 300,
+    right: 10,
+    padding: 8,
+    backgroundColor: "#080808",
+    borderRadius: 8,
   },
   name: {
     fontSize: 24,
@@ -110,14 +147,45 @@ const styles = StyleSheet.create({
     shadowRadius: 3,
   },
   card: {
-    borderRadius: 12
+    paddingTop: 10
   },
   membersGoing: {
     color: "#676464",
     fontSize: 12,
     fontFamily: "IBMPlexSans-Regular",
     marginTop: 15,
-    textDecorationLine: 'underline'
+    textDecorationLine: 'underline',
+    paddingBottom: 10
+  },
+  modal: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingrRight: 20
+  },
+  modalContainer: {
+    backgroundColor: '#fff',
+    padding: 16,
+    borderRadius: 8,
+    width: '80%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  modalOptionsContainer: {
+    width: '100%',
+    alignItems: 'center',
+  },
+  modalOption: {
+    padding: 8,
+    marginBottom: 8,
+    backgroundColor: "#FFC60A",
+    borderRadius: 12,
+    width: 120,
+    alignItems: 'center'
   },
 });
 
