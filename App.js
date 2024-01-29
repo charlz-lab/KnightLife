7e91f0a441fd22917ac92f24451bd1fbb4a28e52
Jessie Jalca <jessiejalca@gmail.com>
2024-01-29 13:38:58 -0500

    move navbar component to its own page

--- App.js
+++ App.js
@@ -1,105 +1,12 @@
-import { StatusBar } from "expo-status-bar"
 import React from "react"
-import { StyleSheet, Text, View, TouchableHighlight, Image } from "react-native"
+import { StyleSheet, Text } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import HOME from "./pages/HOME"
-import PROFILE from "./pages/PROFILE"
 import { EDIT_PROFILE } from "./pages/PROFILE"
-import CREATE_EVENTS from "./pages/CREATE_EVENTS"
 import appStyles from "./styles"
 import { useFonts } from "expo-font"
-const Tab = createBottomTabNavigator()
+import NavBar from "./components/NavBar"
 const Stack = createNativeStackNavigator()
-var isCreator = false
-
-// navigation for nav bar
-function NavBar() {
-  return (
-    <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: { backgroundColor: appStyles.colors.mainBackground },
-      }}>
-      {isCreator ? (
-        <>
-          <Tab.Screen
-            name="Create_Events"
-            component={CREATE_EVENTS}
-            options={{
-              title: "",
-              tabBarIcon: ({ size, focused, color }) => {
-                return (
-                  <Image
-                    style={{ width: size, height: size }}
-                    source={{
-                      uri: require("./assets/icons/fi-br-add.png"),
-                    }}
-                  />
-                )
-              },
-            }}
-          />
-          <Tab.Screen
-            name="Profile"
-            component={PROFILE}
-            options={{
-              title: "",
-              tabBarIcon: ({ size, focused, color }) => {
-                return (
-                  <Image
-                    style={{ width: size, height: size }}
-                    source={{
-                      uri: require("./assets/icons/fi-bs-profile.png"),
-                    }}
-                  />
-                )
-              },
-            }}
-          />
-        </>
-      ) : (
-        <>
-          <Tab.Screen
-            name="Home"
-            component={HOME}
-            options={{
-              title: "",
-              tabBarIcon: ({ size, focused, color }) => {
-                return (
-                  <Image
-                    style={{ width: size, height: size }}
-                    source={{
-                      uri: require("./assets/icons/fi-ss-home.png"),
-                    }}
-                  />
-                )
-              },
-            }}
-          />
-          <Tab.Screen
-            name="Profile"
-            component={PROFILE}
-            options={{
-              title: "",
-              tabBarIcon: ({ size, focused, color }) => {
-                return (
-                  <Image
-                    style={{ width: size, height: size }}
-                    source={{
-                      uri: require("./assets/icons/fi-bs-profile.png"),
-                    }}
-                  />
-                )
-              },
-            }}
-          />
-        </>
-      )}
-    </Tab.Navigator>
-  )
-}
 
 // navigation outside of nav bar
 export default function App() {
