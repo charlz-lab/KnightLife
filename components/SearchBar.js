167bb8c5c00c6911cddab5fddb1f49db8323a523
Jessie Jalca <jessiejalca@gmail.com>
2024-01-31 12:16:12 -0500

    add a clear button to search bar

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,6 +1,15 @@
 import React, { useState } from "react"
-import { View, TextInput, StyleSheet, Image } from "react-native"
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Image,
+  Pressable,
+  Text,
+} from "react-native"
+import { Card, Icon } from "react-native-elements"
 import searchIcon from "../assets/icons/fi-br-search.png"
+import appStyles from "../styles"
 
 const SearchBar = ({ onSearch }) => {
   const [searchText, setSearchText] = useState("")
@@ -12,15 +21,37 @@ const SearchBar = ({ onSearch }) => {
     onSearch(searchText)
   }
 
+  const handleClear = () => {
+    setSearchText("")
+    onSearch("")
+  }
+
   return (
     <View style={styles.container}>
-      <Image source={searchIcon} style={{ height: 20, width: 20 }} />
-      <TextInput
-        style={styles.input}
-        placeholder="Search for events or creator accounts"
-        onChangeText={(text) => setSearchText(text)}
-        onSubmitEditing={handleSearch}
-      />
+      <View
+        style={[
+          appStyles.layout.horizontal,
+          { flex: 5, justifyContent: "flex-start" },
+        ]}>
+        <Image source={searchIcon} style={{ height: 20, width: 20 }} />
+        <TextInput
+          style={styles.input}
+          placeholder="Search for events or creator accounts"
+          onChangeText={(text) => setSearchText(text)}
+          onSubmitEditing={handleSearch}
+          value={searchText}
+        />
+      </View>
+      {searchText.length > 0 && (
+        <Pressable style={styles.clearButton} onPress={handleClear}>
+          <Icon
+            name="close"
+            type="ionicon"
+            size={25}
+            color={appStyles.colors.accent1}
+          />
+        </Pressable>
+      )}
     </View>
   )
 }
@@ -28,19 +59,26 @@ const SearchBar = ({ onSearch }) => {
 const styles = StyleSheet.create({
   container: {
     paddingVertical: 4,
-    paddingHorizontal: 10,
+    paddingHorizontal: 12,
     backgroundColor: "#f0f0f0",
     borderRadius: 100,
     flexDirection: "row",
     alignItems: "center",
     flex: 3,
     marginVertical: 8,
+    justifyContent: "space-between",
   },
   input: {
     height: 40,
     paddingLeft: 10,
-    width: "100%",
     marginRight: 8,
+    flex: 1,
+    textAlign: "left",
+  },
+  clearButton: {
+    marginLeft: 8,
+    flex: 0.5,
+    backgroundColor: "#f0f0f0",
   },
 })
 
