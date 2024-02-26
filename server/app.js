7280df6c696027da9f7f4f5cbbaebc8eb80b8b24
Jessie Jalca <jessiejalca@gmail.com>
2024-02-26 18:40:53 -0500

    switch to mysql2 and add ssh private key

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 // Set up express server
 const express = require("express")
-const mariadb = require("mariadb")
+const mysql2 = require("mysql2")
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -23,6 +23,8 @@ sshClient
     port: process.env.SSH_PORT,
     username: process.env.SSH_USERNAME,
     password: process.env.SSH_PASSWORD,
+    privateKey: require("fs").readFileSync(process.env.SSH_PRIVATE_KEY),
+    // readyTimeout: 99999,
   })
   .addListener("ready", () => {
     console.log("Connected to SSH server!")
@@ -40,47 +42,39 @@ sshClient
 
 // Connect & query the database
 const createDatabaseConnection = async () => {
-  const db = mariadb
-    .createPool({
-      connectionLimit: 10,
+  const db = mysql2
+    .createConnection({
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      allowPublicKeyRetrieval: true,
-    })
-    .addListener("error", (err) => {
-      // console.log("Error connecting to database: " + err)
-    })
-    .addListener("enqueue", () => {
-      console.log("Waiting for available connection slot")
-    })
-    .addListener("release", (connection) => {
-      console.log("Connection released!")
-    })
-    .addListener("close", () => {
-      console.log("Connection to database closed!")
+      // connectTimeout: 99999,
     })
     .addListener("end", () => {
       console.log("Disconnected from database!")
     })
+    .addListener("close", () => {
+      console.log("Connection to database closed!")
+    })
+    .addListener("connect", () => {
+      console.log("Connected to database!")
+    })
+    .addListener("ready", () => {
+      console.log("Database connection ready!")
+    })
 
-  db.getConnection()
-    .then((conn) => {
-      conn.query("SELECT 1 as val").then((rows) => {
-        console.log("Connected to database!")
-        conn.end()
-        createAPIEndpoints(db)
-      })
-    })
-    .catch((err) => {
+  db.connect((err) => {
+    if (err) {
       console.log("Error connecting to database: " + err)
-    })
+    } else {
+      console.log("Connected to database!")
+    }
+  })
 
   // Get all users
-  app.get("/users", (req, res) => {
-    const sql = "SELECT * FROM users"
+  app.get("/users", async (req, res) => {
+    const sql = "SELECT * FROM users;"
     db.query(sql, (err, result) => {
       if (err) {
         res.status(500).send("Error retrieving users from database")
