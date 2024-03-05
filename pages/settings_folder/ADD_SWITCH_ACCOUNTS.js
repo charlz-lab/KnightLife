58e0bf9ed805626052fca210c41597343b6f87f6
bettagliocris3 <156712087+bettagliocris3@users.noreply.github.com>
2024-03-05 18:19:29 +0000

    add/ switch acc styling

--- pages/settings_folder/ADD_SWITCH_ACCOUNTS.js
+++ pages/settings_folder/ADD_SWITCH_ACCOUNTS.js
@@ -3,6 +3,19 @@ import React, { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
 import appStyles from "../../styles";
 
+const userAccounts = [
+  {
+    profilePic: require("../../images/janeDoeProfile.png"),
+    profileName: "Jane Doe",
+    accountType: "Personal",
+  },
+  {
+    profilePic: require("../../images/chessClubPic.png"),
+    profileName: "Chess Club",
+    accountType: "Creator",
+  },
+];
+
 const AddSwitchAccounts = ({ navigation }) => {
   const [activeAccount, setActiveAccount] = useState(1);
   const [accountCount, setAccountCount] = useState(2);
@@ -30,28 +43,45 @@ const AddSwitchAccounts = ({ navigation }) => {
     <View style={styles.container}>
       <Text style={appStyles.fonts.heading}>Add/Switch Accounts</Text>
 
-      {[...Array(accountCount)].map((_, index) => (
+      {userAccounts.map((_, index) => (
         <TouchableOpacity
           key={index}
           style={[
-            appStyles.profileCard,
-            appStyles.shawdowInput,
+            styles.accountCard,
+            appStyles.shadowInput,
             {
               backgroundColor:
-                activeAccount === index + 1 ? "#FFC60A" : "#E2E2E2",
+                activeAccount === index + 1 ? "#FDF5E6" : "white",
+              borderColor: activeAccount === index + 1 ? "#FFC60A" : "white",
             },
           ]}
           onPress={() => switchAccount(index + 1)}
         >
-          <Text style={styles.buttonText1}>Account {index + 1}</Text>
+          <Image source={_.profilePic} style={{ width: 60, height: 60 }} />
+          <View style={{ marginTop: 10 }}>
+            <Text style={[appStyles.fonts.paragraphNoSize, { fontSize: 20 }]}>
+              {_.profileName}
+            </Text>
+            <Text
+              style={[
+                appStyles.fonts.paragraphNoSize,
+                { fontSize: 15, color: "#676464" },
+              ]}
+            >
+              {_.accountType}
+            </Text>
+          </View>
         </TouchableOpacity>
       ))}
 
-      <TouchableOpacity
-        style={appStyles.buttons.buttonClick}
-        onPress={navigateToCreateAccount}
-      >
-        <Text style={styles.buttonText}>Create Account</Text>
+      <TouchableOpacity style={styles.button} onPress={navigateToCreateAccount}>
+        <Image
+          source={require("../../assets/icons/fi-br-add.png")}
+          style={{ width: 25, height: 25 }}
+        />
+        <Text style={[appStyles.fonts.paragraph, { color: "white" }]}>
+          Add Account
+        </Text>
       </TouchableOpacity>
     </View>
   );
@@ -79,6 +109,29 @@ const styles = StyleSheet.create({
     color: "#000",
     fontSize: 16,
     fontWeight: "bold",
+  },
+  accountCard: {
+    width: "85%",
+    borderRadius: 25,
+    paddingVertical: 17,
+    paddingHorizontal: 22,
+    gap: 10,
+    marginVertical: 20,
+    flexDirection: "row",
+    borderWidth: 2,
+  },
+  button: {
+    backgroundColor: "black",
+    width: "50%",
+    borderRadius: 30,
+    paddingVertical: 17,
+    paddingHorizontal: 22,
+    gap: 10,
+    alignItems: "center",
+    marginVertical: 20,
+    flexDirection: "row",
+    alignSelf: "flex-end",
+    marginRight: 25,
   },
 });
 
--- pages/settings_folder/CREATEACCOUNT.js
+++ pages/settings_folder/CREATEACCOUNT.js
@@ -1,17 +1,23 @@
 // CreateAccount.js
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import appStyles from '../../styles';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import appStyles from "../../styles";
 
 const CreateAccount = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigation = useNavigation();
 
   const handleCreateAccount = () => {
     // Handle account creation logic here
-    console.log('Created Account:', { username, password });
+    console.log("Created Account:", { username, password });
 
     // Navigate back to AddSwitchAccounts screen
     navigation.goBack();
@@ -21,22 +27,29 @@ const CreateAccount = () => {
     <View style={styles.container}>
       <Text style={appStyles.fonts.heading}>Add Account</Text>
 
-      <TextInput
-        style={[appStyles.textInput, appStyles.shawdowInput, styles.margin]}
-        placeholder="Enter username"
-        placeholderTextColor="gray"
-        value={username}
-        onChangeText={(text) => setUsername(text)}
-      />
-      <TextInput
-        style={[appStyles.textInput, appStyles.shawdowInput, styles.margin]}
-        placeholder="Enter password"
-        placeholderTextColor="gray"
-        secureTextEntry
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
-      <TouchableOpacity style={appStyles.buttons.yellow} onPress={handleCreateAccount}>
+      <View style={[appStyles.sectionStyle, { marginTop: 20 }]}>
+        <TextInput
+          style={[appStyles.textInput]}
+          placeholder="Enter username"
+          placeholderTextColor="gray"
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+        />
+      </View>
+      <View style={[appStyles.sectionStyle, { marginTop: 20 }]}>
+        <TextInput
+          style={[appStyles.textInput]}
+          placeholder="Enter password"
+          placeholderTextColor="gray"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <TouchableOpacity
+        style={appStyles.buttons.yellow}
+        onPress={handleCreateAccount}
+      >
         <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
     </View>
@@ -46,24 +59,24 @@ const CreateAccount = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'top',
+    alignItems: "center",
+    justifyContent: "top",
     padding: 16,
+    backgroundColor: "white",
   },
   margin: {
     marginTop: 15,
     marginBottom: 15,
   },
 
-
   createButton: {
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 8,
-    backgroundColor: '#2ecc71', // Create Account button color
+    backgroundColor: "#2ecc71", // Create Account button color
   },
   buttonText: {
-    color: '#000',
+    color: "#000",
     fontSize: 15,
     fontFamily: "IBMPlexSans-Medium",
   },
