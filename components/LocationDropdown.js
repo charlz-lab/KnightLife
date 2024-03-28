6bbf446cdc9ceb8144280de3ac2e45bee41c2c4a
bettagliocris3 <156712087+bettagliocris3@users.noreply.github.com>
2024-03-28 22:24:51 +0000

    edit event done, created location dropdown

---
+++ components/LocationDropdown.js
@@ -0,0 +1,100 @@
+import appStyles from "../styles";
+import { StyleSheet, View, Text } from "react-native";
+import SelectDropdown from "react-native-select-dropdown";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+export default function LocationDropdown() {
+  const items = ["Main Campus", "Downtown", "Rosen College", "Cocoa Beach"];
+  return (
+    <SelectDropdown
+      data={items}
+      onSelect={(selectedItem, index) => {
+        console.log(selectedItem, index);
+      }}
+      renderButton={(selectedItem, isOpened) => {
+        return (
+          <View style={styles.dropdownButtonStyle}>
+            <Text
+              style={
+                selectedItem === null
+                  ? styles.dropdownButtonTxtStyleDefault
+                  : styles.dropdownButtonTxtStyle
+              }
+            >
+              {selectedItem || "Location"}
+            </Text>
+            <Icon
+              name={isOpened ? "chevron-up" : "chevron-down"}
+              style={styles.dropdownButtonArrowStyle}
+            />
+          </View>
+        );
+      }}
+      renderItem={(item, index, isSelected) => {
+        return (
+          <View
+            style={{
+              ...styles.dropdownItemStyle,
+              ...(isSelected && { backgroundColor: "#D2D9DF" }),
+            }}
+          >
+            <Text style={styles.dropdownItemTxtStyle}>{item}</Text>
+          </View>
+        );
+      }}
+      showsVerticalScrollIndicator={false}
+      dropdownStyle={styles.dropdownMenuStyle}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  dropdownButtonStyle: {
+    height: 40,
+    marginLeft: 35,
+    marginRight: 35,
+    backgroundColor: "white",
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: "#dadae8",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 12,
+  },
+  dropdownButtonTxtStyle: {
+    flex: 1,
+    fontSize: 16,
+    color: "black",
+    fontFamily: appStyles.fonts.paragraph.fontFamily,
+  },
+  dropdownButtonTxtStyleDefault: {
+    flex: 1,
+    fontSize: 16,
+    color: "#8b9cb5",
+    fontFamily: appStyles.fonts.paragraph.fontFamily,
+  },
+  dropdownButtonArrowStyle: {
+    fontSize: 28,
+    color: "#8b9cb5",
+  },
+  dropdownMenuStyle: {
+    backgroundColor: "#E9ECEF",
+    borderRadius: 10,
+  },
+  dropdownItemStyle: {
+    width: "100%",
+    flexDirection: "row",
+    paddingHorizontal: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 8,
+  },
+  dropdownItemTxtStyle: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#151E26",
+    fontFamily: appStyles.fonts.paragraph.fontFamily,
+  },
+});
--- pages/settings_folder/Link pages/EditEvent.js
+++ pages/settings_folder/Link pages/EditEvent.js
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Pressable, Alert, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  Alert,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+} from "react-native";
 import appStyles from "../../../styles";
+import * as ImagePicker from "expo-image-picker";
+import LocationDropdown from "../../../components/LocationDropdown";
 
 const EditEvents = ({ route, navigation }) => {
   const { event, onEventUpdate } = route.params;
+  const [image, setImage] = useState(null);
 
   const [updatedEventDetails, setUpdatedEventDetails] = useState({
     name: event.name,
-    dateTime: event.dateTime,
     location: event.location,
+    buildingRoomNum: event.buildingRoomNum,
+    dateTime: event.dateTime,
     description: event.description,
   });
 
@@ -20,107 +34,183 @@ const EditEvents = ({ route, navigation }) => {
   };
 
   const saveAlert = () =>
-    Alert.alert("Save Changes", "Are you sure you would like to save your changes?", [
-      {
-        text: "Cancel",
-        onPress: () => console.log("Cancel Pressed"),
-        style: "cancel",
-      },
-      {
-        text: "Save",
-        onPress: () => {
-          const updatedEvent = {
-            ...event,
-            name: updatedEventDetails.name,
-            dateTime: updatedEventDetails.dateTime,
-            location: updatedEventDetails.location,
-            description: updatedEventDetails.description,
-          };
-
-          onEventUpdate(updatedEvent);
-          navigation.goBack();
+    Alert.alert(
+      "Save Changes",
+      "Are you sure you would like to save your changes?",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
         },
-      },
-    ]);
+        {
+          text: "Save",
+          onPress: () => {
+            const updatedEvent = {
+              ...event,
+              name: updatedEventDetails.name,
+              location: updatedEventDetails.location,
+              buildingRoomNum: event.buildingRoomNum,
+              dateTime: updatedEventDetails.dateTime,
+              description: updatedEventDetails.description,
+            };
+
+            onEventUpdate(updatedEvent);
+            navigation.goBack();
+          },
+        },
+      ]
+    );
+
+  // handle image upload
+  let openImagePickerAsync = async () => {
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [0, 0],
+    });
+
+    if (pickerResult.canceled === true) {
+      return;
+    }
+
+    setImage({ uri: pickerResult.assets[0].uri });
+  };
 
   return (
-    <View style={styles.container}>
-      <Text style={appStyles.fonts.subHeading}>Name:</Text>
-      <View style={appStyles.sectionStyle}>
-        <TextInput
-          value={updatedEventDetails.name}
-          onChangeText={(value) => handleChange("name", value)}
-          style={[appStyles.fonts.paragraph, appStyles.textInput]}
-          placeholder="Event Name"
-          placeholderTextColor={"black"}
-  />
-      </View>
+    <ScrollView
+      contentContainerStyle={{ paddingBottom: 500, backgroundColor: "white" }}
+    >
+      <View style={styles.container}>
+        <View style={styles.imageBanner}>
+          {image === null ? (
+            <Text
+              style={[
+                appStyles.fonts.paragraph,
+                {
+                  textAlign: "center",
+                  color: "#8b9cb5",
+                },
+              ]}
+            >
+              No image uploaded
+            </Text>
+          ) : (
+            <Image source={image} style={styles.imageUpload} />
+          )}
+        </View>
+        <TouchableOpacity onPress={openImagePickerAsync}>
+          <Text
+            style={[
+              appStyles.fonts.paragraph,
+              { textDecorationLine: "underline" },
+            ]}
+          >
+            Click to upload image
+          </Text>
+        </TouchableOpacity>
+        <Text style={appStyles.fonts.subHeading}>Name:</Text>
+        <View style={appStyles.sectionStyle}>
+          <TextInput
+            value={updatedEventDetails.name}
+            onChangeText={(value) => handleChange("name", value)}
+            style={[appStyles.fonts.paragraph, appStyles.textInput]}
+            placeholder="Event Name"
+            placeholderTextColor={"black"}
+          />
+        </View>
 
-    
+        <Text style={appStyles.fonts.subHeading}>Location:</Text>
+        <LocationDropdown></LocationDropdown>
+        <Text style={appStyles.fonts.subHeading}>Building & Room Number:</Text>
+        <View style={appStyles.sectionStyle}>
+          <TextInput
+            value={updatedEventDetails.buildingRoomNum}
+            onChangeText={(value) => handleChange("buildingRoomNum", value)}
+            style={[appStyles.fonts.paragraph, appStyles.textInput]}
+            placeholder="Building & Room Number"
+            placeholderTextColor={"black"}
+          />
+        </View>
 
-<Text style={appStyles.fonts.subHeading}>Date and Time:</Text>
-<View style={appStyles.sectionStyle}>
-        <TextInput
-          value={updatedEventDetails.dateTime}
-          onChangeText={(value) => handleChange("dateTime", value)}
-          style={[appStyles.fonts.paragraph, appStyles.textInput]}
-          placeholder="Date & Time"
-          placeholderTextColor={"black"}
-  />
-      </View>
-      
+        <Text style={appStyles.fonts.subHeading}>Date and Time:</Text>
+        <View style={appStyles.sectionStyle}>
+          <TextInput
+            value={updatedEventDetails.dateTime}
+            onChangeText={(value) => handleChange("dateTime", value)}
+            style={[appStyles.fonts.paragraph, appStyles.textInput]}
+            placeholder="Date & Time"
+            placeholderTextColor={"black"}
+          />
+        </View>
 
-<Text style={appStyles.fonts.subHeading}>Location:</Text>
-<View style={appStyles.sectionStyle}>
-        <TextInput
-          value={updatedEventDetails.location}
-          onChangeText={(value) => handleChange("location", value)}
-          style={[appStyles.fonts.paragraph, appStyles.textInput]}
-          placeholder="Location"
-          placeholderTextColor={"black"}
-  />
-      </View>
+        <Text style={appStyles.fonts.subHeading}>Description:</Text>
+        <View style={appStyles.sectionStyle}>
+          <TextInput
+            value={updatedEventDetails.description}
+            onChangeText={(value) => handleChange("description", value)}
+            style={[appStyles.fonts.paragraph, appStyles.textInput]}
+            multiline
+            placeholder="Description"
+            placeholderTextColor={"black"}
+          />
+        </View>
 
-
-<Text style={appStyles.fonts.subHeading}>Description:</Text>
-<View style={appStyles.sectionStyle}>
-        <TextInput
-          value={updatedEventDetails.description}
-          onChangeText={(value) => handleChange("description", value)}
-          style={[appStyles.fonts.paragraph, appStyles.textInput]}
-          multiline
-          placeholder="Description"
-          placeholderTextColor={"black"}
-  />
-      </View>
-      
-
-      {/* Button to update the event and close the screen */}
-      <View style={{ flexDirection: "row", columnGap: 5, marginTop: 20 }}>
+        {/* Button to update the event and close the screen */}
+        <View style={{ flexDirection: "row", columnGap: 5, marginTop: 20 }}>
           <Pressable
             style={[appStyles.buttons.yellow, appStyles.shadow]}
-            onPress={saveAlert}>
+            onPress={saveAlert}
+          >
             <Text style={appStyles.fonts.paragraph}>Save</Text>
           </Pressable>
           <Pressable
             style={[appStyles.buttons.black, appStyles.shadow]}
-            onPress={() => navigation.goBack()}>
-            <Text style={[{ color: "white" }, appStyles.fonts.paragraph]}> Cancel </Text>
+            onPress={() => navigation.goBack()}
+          >
+            <Text style={[{ color: "white" }, appStyles.fonts.paragraph]}>
+              {" "}
+              Cancel{" "}
+            </Text>
           </Pressable>
         </View>
-    </View>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     flexDirection: "column",
-    rowGap: 45,
+    rowGap: 20,
     backgroundColor: appStyles.colors.background,
     alignItems: "center",
+    paddingTop: 20,
   },
-  
-})
+  imageBanner: {
+    marginTop: 20,
+    width: "80%%",
+    height: "25%",
+    alignItems: "center",
+    alignSelf: "center",
+    borderRadius: 30,
+    borderColor: "#dadae8",
+    borderWidth: 1,
+    justifyContent: "center",
+  },
+  imageUpload: {
+    width: "100%%",
+    height: "100%",
+    borderRadius: 30,
+  },
+});
 
 export default EditEvents;
